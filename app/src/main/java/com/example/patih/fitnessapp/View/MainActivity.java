442975443400998fd20aee440b546b02e2a42a64
Patrik I <260010@via.dk>
2018-10-29 10:56:05 +0100

    fck this stupid studio

--- app/src/main/java/com/example/patih/fitnessapp/View/MainActivity.java
+++ app/src/main/java/com/example/patih/fitnessapp/View/MainActivity.java
@@ -5,7 +5,8 @@ import android.app.Activity;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.support.v13.app.FragmentPagerAdapter;
+//import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;

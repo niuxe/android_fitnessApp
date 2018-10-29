ce2564d17bd05c4357b84c8eecac0a91c7af4866
Kristian Dam Hess Kristensen <kristian.d.kristensen@hotmail.com>
2018-10-29 10:14:29 +0100

    Packages created

--- app/src/main/java/com/example/patih/fitnessapp/View/MainActivity.java
+++ app/src/main/java/com/example/patih/fitnessapp/View/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.patih.fitnessapp;
+package com.example.patih.fitnessapp.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.patih.fitnessapp.R;
 
 public class MainActivity extends AppCompatActivity {
 

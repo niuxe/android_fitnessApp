30532c2de898b3ae39261970c454b52eeac5027c
Zahata <37177462+Zahata@users.noreply.github.com>
2018-11-05 10:30:53 +0100

    added all 3 fragments and we can switch between them

---
+++ app/src/main/java/com/example/krist/myapplication/Fragment1_Calendar.java
@@ -0,0 +1,16 @@
+package com.example.krist.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1_Calendar extends Fragment{
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.calendar_fragment, container, false);
+        return rootView;
+    }
+}

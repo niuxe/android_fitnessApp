3ad6fcd14a92fef18bcfeda351623a5f691066aa
Kristian Dam Hess Kristensen <kristian.d.kristensen@hotmail.com>
2018-11-12 10:29:20 +0100

    refactored

--- app/src/main/java/com/example/krist/myapplication/Fragment3_Graph.java
+++ app/src/main/java/com/example/krist/myapplication/Fragment3_Graph.java
@@ -6,11 +6,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class Fragment3_Graph extends Fragment {
+
+    ArrayList<Exercises> exercises = new ArrayList<>();
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.graph_fragment, container, false);
+
+        for (Exercises E: exercises) {
+
+        }
+
         return rootView;
     }
 }
